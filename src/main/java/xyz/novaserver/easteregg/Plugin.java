a94dbec9d855d8ba8395d0ef67bab754b7784839
wilson048 <48695053+wilson048@users.noreply.github.com>
2020-10-15 00:33:15 -0500

    Project setup

---
+++ src/main/java/xyz/novaserver/easteregg/Plugin.java
@@ -0,0 +1,11 @@
+package xyz.novaserver.easteregg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Plugin extends JavaPlugin
+{
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
